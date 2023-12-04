4336118ea876d5cce84bb98475b021f89ae0cf44
Maria Kulo <mariavkulo@gmail.com>
2023-12-04 14:45:07 +0200

    Nuget packages added

---
+++ src/New.Namespace/MarianTietokanta21223Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace src.New.Namespace;
+
+public partial class MarianTietokanta21223Context : DbContext
+{
+    public MarianTietokanta21223Context()
+    {
+    }
+
+    public MarianTietokanta21223Context(DbContextOptions<MarianTietokanta21223Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:marian-serveri-4-12.database.windows.net,1433;Initial Catalog=Marian tietokanta 2.12.23;Persist Security Info=False;User ID=CloudSAdaa3d165;Password=StrongPassword8;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Persons__3214EC07D4CCBDEC");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .HasColumnName("firstName");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .HasColumnName("lastName");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
